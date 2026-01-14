51623ea9047cc96461eab3a121beecbb3de2cc29
youys90 <siksik01@naver.com>
2026-01-15 08:11:47 +0900

    db이관작업(스탬프)

--- adm/next.config.ts
+++ adm/next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "eftuvzzadxxtxzpgfqom.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
     ],
   },
 };
